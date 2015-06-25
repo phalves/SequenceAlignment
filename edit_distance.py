39b7c537062f37e9f0eb00cb6d53d1fa928c14a5
fernandus16 <fernando.correia@jusbrasil.com.br>
2015-06-25 00:00:58 -0300

    Updates notebook

--- edit_distance.py
+++ edit_distance.py
@@ -3,7 +3,7 @@ from copy import deepcopy
 import sys
 
 
-sys.setrecursionlimit(200000)
+sys.setrecursionlimit(2**30)
 
 
 def sequence_alignemt(x, y, g, a):
@@ -46,8 +46,6 @@ def get_sequence(x, y, g, a, M):
                 a   - Mismatch penalty
                 M   - Matrix of cost
     """
-    
-    print "x: %s, y: %s " % (x, y)
     diff = [[], []]
     find_sequence(len(x) - 1, len(y) - 1, x, y, g, a, M, diff)
     diff[0].reverse()
@@ -57,11 +55,11 @@ def get_sequence(x, y, g, a, M):
 
 def find_sequence(i, j, x, y, g, a, M, diff):
     if i < 0:
-        for i in reversed(range(len(y) - 1)):
+        for i in reversed(range(j + 1)):
             _add_to_diff(diff, "*", y[i])
         return
     elif j< 0:
-        for i in reversed(range(len(x) - 1)):
+        for i in reversed(range(i + 1)):
             _add_to_diff(diff, x[i], "*")
         return
     if i > 0 or j > 0:
@@ -79,7 +77,10 @@ def find_sequence(i, j, x, y, g, a, M, diff):
             _add_to_diff(diff, "*", y[j])
             return find_sequence(i, j - 1, x, y, g, a, M, diff)
     elif j == 0 and i == 0:
-        _add_to_diff(diff, x[i], y[j])
+        if x[i] == y[j]:
+            _add_to_diff(diff, x[i], y[j])
+        else:
+            _add_to_diff(diff, "<%s>" % x[i], "<%s>" % y[j])
 
 def _add_to_diff(diff, x, y):
     diff[0].append(x)
@@ -110,26 +111,37 @@ def linear_sequence_alignment(x, y, g, a):
     return CURRENT, CURRENT[m - 1]
 
 
-def divide_and_conquer_alignment_1(x, y, g, a):
+def get_sequence_linear(x, y, g, a):
+    diff_max_size = len(x) + len(y)
+    diff = [["" for _ in range(diff_max_size)] for __ in range(2)]
+    divide_and_conquer_alignment_1(x, y, g, a, diff, diff_max_size / 2, diff_max_size / 2)
+    return "".join(diff[0]), "".join(diff[1])
+
+
+def divide_and_conquer_alignment_1(x, y, g, a, diff, offset, index):
     m = len(x)
     n = len(y)
 
     if m >= 0 and n >= 0:
         if m <= 2 or n <= 2:
-            print optmal_alignment(x, y, g, a)
+            _diff = optmal_alignment(x, y, g, a)
+            diff[0][index] = _diff[0]
+            diff[1][index] = _diff[1]
         else:
             _f, _ = linear_sequence_alignment(x, y[:int(n/2)], g, a)
             _g, _ = linear_sequence_alignment(x[::-1], y[int(n/2):][::-1], g, a)
-            r = [_f[i] + _g[i] for i in range(len(_f))]
-            print r
-            q = r.index(min(r))
-            n = len(r)
-            print "q", q
-            print "xq", x[q]
-            print ">-", x[:q+1], y[:n/2]
-            divide_and_conquer_alignment_1(x[:q+1], y[:int(ceil(n / 2.0))], g, a)
-            print ">--", x[q+1:], y[n / 2:]
-            divide_and_conquer_alignment_1(x[q+1:], y[int(ceil(n / 2.0)):], g, a)
+            _, c = linear_sequence_alignment(x, y, g, a)
+            r = [int((_f[i] + _g[i])*10) for i in range(len(_f))]
+#             q = (find_last(r, min(r))) if int(c*10) not in r else find_last(r, int(c*10))
+            q = (r.index(min(r))) if int(c*10) not in r else r.index(int(c*10))
+            divide_and_conquer_alignment_1(x[:q], y[:n / 2], g, a, diff, offset / 2 , index - offset / 2)
+            divide_and_conquer_alignment_1(x[q:], y[n / 2:], g, a, diff, offset / 2, index + offset / 2)
+
+
+def find_last(l, value):
+    x = deepcopy(l)
+    x.reverse()
+    return len(x) - x.index(value) - 1
 
 
 def optmal_alignment(x, y, g, a):
